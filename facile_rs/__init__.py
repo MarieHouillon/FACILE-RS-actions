b042c01a7bf10cda1a8cb4fdf0e953b9658dc598
Jochen Klar <mail@jochenklar.de>
2024-02-27 18:28:06 +0100

    Add pyproject.toml

--- facile_rs/__init__.py
+++ facile_rs/__init__.py
@@ -1,8 +1,1 @@
-__title__ = 'FACILE-RS'
-__version__ = '1.0'
-__author__ = 'Jochen Klar'
-__email__ = 'mail@jochenklar.de'
-__license__ = 'Apache-2.0'
-__copyright__ = 'Copyright (c) 2020 Karlsruhe Institute for Technology (KIT)'
-
-VERSION = __version__
+VERSION = __version__ = '1.0'
