8ce3327482b45145496ff2156a7c2b5a493c1470
Axel Loewe <axel.loewe@kit.edu>
2024-09-28 18:22:30 +0000

    Update radar.py: license name without URL

--- facile_rs/utils/metadata/radar.py
+++ facile_rs/utils/metadata/radar.py
@@ -281,13 +281,12 @@ class RadarMetadata:
         if 'license' in self.data:
             licenseName = ""
             if isinstance(self.data['license'], str):
-                licenseName = self.data['license']
-                # CodeMeta wants the license to be a URL or a creative work, RADAR is only interested in the name
-                if licenseName.startswith("https://spdx.org/licenses/"):
-                    licenseName.replace("https://spdx.org/licenses/", "")
+                licenseName = self.data['license']                
             elif isinstance(self.data['license'], dict) and 'name' in self.data['license']:
                 licenseName = self.data['license']['name']
-
+            # CodeMeta wants the license to be a URL or a creative work, RADAR is only interested in the name
+            if licenseName.startswith("https://spdx.org/licenses/"):
+                licenseName.replace("https://spdx.org/licenses/", "")
             for spdx_name, radar_name in self.radar_licenses.items():
                 if spdx_name:
                     licenseName = licenseName.replace(spdx_name, radar_name)
